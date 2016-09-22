f6778e3149c8b845ec770846e4c941ea7a12d4a7
JeffLupia <lupiaj@gmail.com>
2016-09-22 03:43:24 -0400

    Add files via upload

---
+++ LAb3_lupia.sql
@@ -0,0 +1,44 @@
+﻿/*    QUESTION 1   */
+SELECT ordnum, totalUSD
+FROM orders;
+
+/*    QUESTION 2   */
+SELECT name, city
+FROM Agents
+WHERE name = 'Smith';
+
+/*    QUESTION 3   */
+SELECT pid, name, priceUSD
+FROM Products
+WHERE quantity > 201000;
+
+ /*    QUESTION 4   */
+SELECT name, city
+FROM Customers
+WHERE city = 'Duluth';
+
+/*    QUESTION 5   */
+SELECT name
+FROM Agents
+WHERE city != 'New York' AND city != 'Duluth';
+
+/*    QUESTION 6   */
+SELECT *
+FROM Products
+WHERE city != 'Dallas' AND city != 'Duluth' AND priceUSD > 1.00;
+
+/*    QUESTION 7   */
+SELECT *
+FROM Orders
+WHERE mon = 'feb' OR mon = 'mar';
+
+/*    QUESTION 8   */
+SELECT *
+FROM Orders
+WHERE mon = 'feb' AND totalUSD >= 600;
+
+/*    QUESTION 9   */
+SELECT *
+FROM Orders
+WHERE cid = 'c005';
+
