8a77394e5e4e52dcf870425d9eb223a59b49bae2
JeffLupia <lupiaj@gmail.com>
2016-12-02 12:55:54 -0500

    Add files via upload

---
+++ Lab10_lupia.sql
@@ -0,0 +1,53 @@
+﻿SELECT *
+FROM Courses
+INNER JOIN Prerequisites on Courses.num = Prerequisites.CourseNum ;
+
+
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+	cidNum int := $1;
+	
+	resultset REFCURSOR := $2;
+   
+begin
+
+   open resultset for
+   
+		SELECT name AS CourseName, num AS CourseNum, preReqNum
+		FROM Courses c INNER JOIN Prerequisites p on c.num = p.courseNum
+		WHERE cidNum = courseNum;
+		
+   return resultset;
+   
+end;
+$$ 
+language plpgsql;
+
+SELECT PreReqsFor(220, 'results');
+Fetch all from results;
+
+
+
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   cidNum int := $1;	
+   resultset REFCURSOR := $2;
+begin
+
+   open resultset for
+   
+      SELECT name AS CourseName, num AS CourseNum, preReqNum
+      FROM Courses c INNER JOIN Prerequisites p on c.num = p.courseNum
+      WHERE cidNum = preReqNum;
+      
+   return resultset;
+   
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(308, 'results');
+
+Fetch all from results;
